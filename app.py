7ba2b808af4074463fe04065a988ac99620ebec1
umutkeskin <ukeskinatkapsul@gmail.com>
2022-09-10 10:56:17 +0300

    trying to do something new.

--- app.py
+++ app.py
@@ -3,20 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///linky.db"
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Link(db.Model):
+    __tablename__ = 'link'
     id = db.Column(db.Integer, primary_key = True)
-    case_number = db.Column(db.String)
-    content = db.Column(db.String(200), nullable = False)
+    link = db.Column(db.String)
+    link_name = db.Column(db.String(200), nullable = False)
+    link_description = db.Column(db.String(200), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
-    date_deadline = db.Column(db.DateTime)
 
-    def __init__(self, case_number, content, date_deadline):
-        self.case_number = case_number 
-        self.content = content
-        self.date_deadline = date_deadline
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key = True)
+    category = db.Column(db.String(200))
+    
+
+    def __init__(self, link, link_name, link_description, category):
+        self.link = link 
+        self.link_name = link_name
+        self.link_description = link_description
+        self.category = category
 
     #def __repr__(self):
     #    return "<Task %r>" % self.id 
@@ -24,50 +32,57 @@ class Todo(db.Model):
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == 'POST':
-      if not request.form['case_number'] or not request.form['content'] or not request.form['date_deadline']:
+      if not request.form['link'] or not request.form['link_name'] or not request.form['link_description'] or not request.form['category']:
          flash('Please enter all the fields', 'error')
       else:
-         task = Todo( case_number = request.form['case_number'], content = request.form['content'],
-           date_deadline = datetime.strptime(request.form["date_deadline"], "%Y-%m-%d"))
+         link = Link( link = request.form['link'], link_name = request.form['link_name'],
+           link_description = request.form['link_description'])
+
+         link = Category(category = request.form['category'])
          
-         db.session.add(task)
+         db.session.add(link)
+         db.session.add(category)
          db.session.commit()
          #flash('Record was successfully added')
          return redirect('/')
          
-    tasks = Todo.query.order_by(Todo.date_created).all()
-    return render_template('index.html', tasks = tasks)
+    links = Link.query.order_by(Todo.date_created).all()
+    categories = Category.query.order_by(Category.id).all()
+    return render_template('index.html', [tasks = tasks, categories =categories])
 
 
-@app.route("/delete/<int:id>")
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+#@app.route("/delete/<int:id>")
+#def delete(id):
+#    task_to_delete = Todo.query.get_or_404(id)#
 
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect("/")
-    except:
-        return "There was a problem deleting that task"
+#    try:
+#        db.session.delete(task_to_delete)
+#        db.session.commit()
+#        return redirect("/")
+#    except:
+#        return "There was a problem deleting that task"#
 
-@app.route("/update/<int:id>", methods = ["GET", "POST"])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method == "POST":
-        task.content = request.form["content"]
-        date_dl = datetime.strptime(request.form["date_deadline"], "%Y-%m-%d")
-        task.date_deadline = date_dl  
+#@app.route("/update/<int:id>", methods = ["GET", "POST"])
+#def update(id):
+#    task = Todo.query.get_or_404(id)
+#    if request.method == "POST":
+#        task.link_name = request.form["link_name"]
+#        date_dl = datetime.strptime(request.form["date_deadline"], "%Y-%m-%d")
+#        task.date_deadline = date_dl  #
 
-        try:
-            db.session.commit()
-            return redirect("/")
-        except:
-            return "There was an issue updating your task"
+#        try:
+#            db.session.commit()
+#            return redirect("/")
+#        except:
+#            return "There was an issue updating your task"#
 
-    else:
-         return render_template("update.html", task = task)  
+#    else:
+#         return render_template("update.html", task = task)  
 
 
 if __name__ == '__main__':
    db.create_all()
    app.run(debug = True)
+
+
+
